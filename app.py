2e4b42e2435c3bed4eb6c9ee2f645e91cb4f16f5
plaiza <52192581+plaiza@users.noreply.github.com>
2019-08-27 14:25:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,9 @@ def callback():
     json_line = request.get_json()
     json_line = json.dumps(json_line)
     decoded = json.loads(json_line)
-    user = decoded["events"][0]['replyToken']
+    user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
+    userText = decoded['queryResult']['intent']['displayName']
+    #user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
     #sendText(user,userText)
     if (userText == 'หวัดดีจ้า') :
